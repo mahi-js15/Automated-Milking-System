81c3cdd1d00ee8d441022316312b432a5d8e58bc
mahi-js15 <mahij0658@gmail.com>
2026-05-22 16:01:27 +0530

    Update Reports.py

--- pages/pages/pages/Reports.py
+++ pages/pages/pages/Reports.py
@@ -1,15 +1,31 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-st.title("🥛 Milk Record")
+st.title("📊 Milk Production Reports")
 
-cow_id = st.text_input("Cow ID")
+data = {
+    "Day":["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    "Milk (L)":[12,15,18,16,20,22,19]
+}
 
-milk = st.number_input(
-    "Milk Quantity (Liters)",
-    min_value=0.0
+df = pd.DataFrame(data)
+
+st.subheader("Weekly Milk Production")
+
+fig = px.bar(
+    df,
+    x="Day",
+    y="Milk (L)",
+    color="Milk (L)",
+    title="Milk Collection Report"
 )
 
-if st.button("Save Record"):
-    st.success(
-        f"Milk record saved: {milk} L from {cow_id}"
-    )
+st.plotly_chart(fig)
+
+total = df["Milk (L)"].sum()
+
+st.metric(
+    label="Total Milk Collected",
+    value=f"{total} L"
+)
