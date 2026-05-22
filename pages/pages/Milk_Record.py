d5b11038667e3477308c391322c9ca74ccab7c7f
mahi-js15 <mahij0658@gmail.com>
2026-05-22 16:02:04 +0530

    Update Milk_Record.py

--- pages/pages/Milk_Record.py
+++ pages/pages/Milk_Record.py
@@ -1,33 +1,15 @@
 import streamlit as st
 
-st.title(
-"🥛 Milk Record"
+st.title("🥛 Milk Record")
+
+cow_id = st.text_input("Cow ID")
+
+milk = st.number_input(
+    "Milk Quantity (Liters)",
+    min_value=0.0
 )
 
-cow = st.text_input(
-"Cow ID"
-)
-
-morning = st.number_input(
-"Morning Milk (L)"
-)
-
-evening = st.number_input(
-"Evening Milk (L)"
-)
-
-total = morning + evening
-
-st.write(
-"Total Milk:",
-total,
-"L"
-)
-
-if st.button(
-"Save Record"
-):
-
+if st.button("Save Record"):
     st.success(
-    "Saved"
+        f"Milk record saved: {milk} L from {cow_id}"
     )
